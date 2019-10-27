4fd71a74474d95fba00b249e944c109e96c815c5
shiller777 <lalala@lala.la>
2019-10-27 21:05:38 +0300

    opposite points algo

--- src/main/java/com/http/shiller/cubeDiagonals/Main.java
+++ src/main/java/com/http/shiller/cubeDiagonals/Main.java
@@ -7,13 +7,20 @@ public class Main {
 //        printDiag2(9);
 //        printDiag3(9);
 //        printDiag4(9);
+        printOppositePoints(9, new Point(9,9,9));
     }
 
 
     private static void printOppositePoints(int cubeSide, Point point) {
-//        point.x;
-//        point.y;
-//        point.z;
+        for (int i = 1; i <= cubeSide; i+=(cubeSide - 1)) {
+            for (int j = 1; j <= cubeSide; j+=(cubeSide - 1)) {
+                for (int k = 1; k <= cubeSide; k+=(cubeSide - 1)) {
+                    if (!(i == point.x && j == point.y && k == point.z)) {
+                        System.out.println(new Point(i, j, k));
+                    }
+                }
+            }
+        }
     }
 
     public static void printDiag1(int cubeSide) {
