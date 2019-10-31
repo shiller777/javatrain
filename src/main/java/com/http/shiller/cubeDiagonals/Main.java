a045bee69cdfbc524db74634c09e102d7cc83fcf
shiller777 <lalala@lala.la>
2019-11-01 02:51:49 +0300

    monster logger * added new logger * added file logging support * need to close files

--- src/main/java/com/http/shiller/cubeDiagonals/Main.java
+++ src/main/java/com/http/shiller/cubeDiagonals/Main.java
@@ -2,9 +2,12 @@ package com.http.shiller.cubeDiagonals;
 
 import com.http.shiller.logger.BaseLogger;
 import com.http.shiller.logger.Logger;
+import com.http.shiller.logger.MonsterLogger;
 
 public class Main {
     private static Logger logger = new BaseLogger();
+    private static Logger logger2 = new MonsterLogger();
+    private static Logger logger3 = new MonsterLogger(true, "C:\\Test\\lalala.txt", "C:\\Test\\ololol.txt");
     public static void main(String[] args) {
 
 //        printDiag1(9);
@@ -17,6 +20,9 @@ public class Main {
 
     private static void printOppositePoints(int cubeSide, Point point) {
         logger.warn("shitcode detected!");
+        logger2.info("ololol");
+        logger3.warn("doesn't work");
+        logger3.info("now works!");
         for (int i = 1; i <= cubeSide; i += (cubeSide - 1)) {
             for (int j = 1; j <= cubeSide; j += (cubeSide - 1)) {
                 for (int k = 1; k <= cubeSide; k += (cubeSide - 1)) {
---
+++ src/main/java/com/http/shiller/logger/MonsterLogger.java
@@ -0,0 +1,113 @@
+package com.http.shiller.logger;
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MonsterLogger extends AbstractLogger {
+
+    private List<OutputStream> streamsToLog = new ArrayList<>();
+    {
+        streamsToLog.add(System.out);
+    }
+
+    public void addOutputStream(OutputStream outputStream) {
+        streamsToLog.add(outputStream);
+    }
+    public void addPrintFile(String filePath, boolean append) {
+        try {
+            boolean fileOk = true;
+            if (!new File(filePath).exists()) {
+                fileOk = new File(filePath).createNewFile();
+            }
+            if (!fileOk) {
+                throw new RuntimeException(String.format("Cannot create file '%s'", filePath));
+            }
+            //@Cleanup
+            FileOutputStream fileStream = new FileOutputStream(filePath, append);
+            streamsToLog.add(fileStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(String.format("File '%s' doesn't exist", filePath));
+        }
+    }
+    public void clearOutputStreamList() {
+        streamsToLog = new ArrayList<>();
+        streamsToLog.add(System.out);
+    }
+
+    public MonsterLogger() {
+        ;
+    }
+
+    public MonsterLogger(boolean append, String... filePaths) {
+        List<String> filePathsList = Arrays.asList(filePaths);
+
+//        filePathsList.forEach(o -> {
+//            if (new File(o).exists()) {
+//                try {
+//                    streamsToLog.add(new FileOutputStream(o));
+//                } catch (FileNotFoundException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                throw new RuntimeException(String.format("File '%s' doesn't exist", o));
+//            }
+//        });
+
+        filePathsList.forEach(o -> {
+            try {
+                boolean fileOk = true;
+                if (!new File(o).exists()) {
+                    fileOk = new File(o).createNewFile();
+                }
+                if (!fileOk) {
+                    throw new RuntimeException(String.format("Cannot create file '%s'", o));
+                }
+                //@Cleanup
+                FileOutputStream fileStream = new FileOutputStream(o, append);
+                streamsToLog.add(fileStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException(String.format("File '%s' doesn't exist", o));
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void log(LogTypes type, String message) {
+        StringBuilder builder = new StringBuilder();
+        builder
+                .append(new Timestamp(System.currentTimeMillis()))
+                .append(" [MONSTER ")
+                .append(type.name())
+                .append("]: ")
+                .append(message);
+
+        for (OutputStream o : streamsToLog) {
+            if (o instanceof PrintStream) {
+                PrintStream printStream = (PrintStream)o;
+                printStream.println(builder.toString());
+            } else {
+                if (o instanceof FileOutputStream) {
+                    try {
+                        byte b[] = builder.toString().getBytes();
+                        byte end[] = "\n".getBytes();
+                        o.write(b);
+                        o.write(end);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                } else {
+                    throw new RuntimeException(
+                            String.format("Only PrintStream is supported for now but not '%s'", o.getClass()));
+                }
+            }
+        }
+    }
+}
