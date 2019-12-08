cdb366f4af6869dead34e2de21e076f0dc8d2911
shiller777 <lalala@lala.la>
2019-12-09 01:34:52 +0300

    opposite side algo update

--- src/main/java/com/http/shiller/cubeDiagonals/Main.java
+++ src/main/java/com/http/shiller/cubeDiagonals/Main.java
@@ -23,15 +23,21 @@ public class Main {
 
         printOppositePoints(9, new Point(9, 9, 9));
 
+        System.out.println("==============================================");
+        printOppositePoints3(9, new Point(2, 2, 1));
+        System.out.println("==============================================");
+        printOppositePoints3(9, new Point(8, 8, 9));
+
+        System.out.println("==============================================");
+        printOppositePoints3(9, new Point(9, 9, 9));
+        System.out.println("==============================================");
+        printOppositePoints3(9, new Point(1, 1, 1));
+
     }
 
 
-    private static void printOppositePoints(int cubeSide, Point point) {
-//        logger.warn("shitcode detected!");
-//        logger2.info("ololol");
-//        logger3.warn("doesn't work");
-//        logger4.warn("doesn't work");
-//        logger3.info("now works!");
+    public static void printOppositePoints(int cubeSide, Point point) {
+
         loggerGodzilla.info("now works!");
         loggerGodzilla.info("asdf");
         loggerGodzilla.info("qwerty");
@@ -51,6 +57,79 @@ public class Main {
             }
         }
     }
+
+
+    public static void printOppositePoints3(int cubeSide, Point point) {
+
+        int baseX;
+        int baseY;
+        int baseZ;
+
+        int offsetX;
+        int offsetY;
+        int offsetZ;
+
+        int multiplierX;
+        int multiplierY;
+        int multiplierZ;
+
+
+        if (point.x > cubeSide / 2) {
+            baseX = cubeSide;
+            offsetX = baseX - point.x;
+        } else {
+            baseX = 1;
+            offsetX = point.x - baseX;
+        }
+
+        if (point.y > cubeSide / 2) {
+            baseY = cubeSide;
+            offsetY = baseY - point.y;
+        } else {
+            baseY = 1;
+            offsetY = point.y - baseY;
+        }
+        if (point.z > cubeSide / 2) {
+            baseZ = cubeSide;
+            offsetZ = baseZ - point.z;
+        } else {
+            baseZ = 1;
+            offsetZ = point.z - baseZ;
+        }
+
+        for (int i = 1; i <= cubeSide; i += (cubeSide - 1)) {
+            for (int j = 1; j <= cubeSide; j += (cubeSide - 1)) {
+                for (int k = 1; k <= cubeSide; k += (cubeSide - 1)) {
+                    if (!(i == baseX && j == baseY && k == baseZ)) {
+                        //logger.info("Base " + new Point(i, j, k).toString());
+
+                        if (i == cubeSide) {
+                            multiplierX = -1;
+                        } else {
+                            multiplierX = 1;
+                        }
+                        if (j == cubeSide) {
+                            multiplierY = -1;
+                        } else {
+                            multiplierY = 1;
+                        }
+                        if (k == cubeSide) {
+                            multiplierZ = -1;
+                        } else {
+                            multiplierZ = 1;
+                        }
+
+                        logger.info("Expected " +
+                                new Point(i + multiplierX * offsetX,
+                                        j + multiplierY * offsetY,
+                                        k + multiplierZ * offsetZ).toString());
+
+                    }
+                }
+            }
+        }
+    }
+
 
     public static void printDiag1(int cubeSide) {
         System.out.println("Cube diagonal 1:");
